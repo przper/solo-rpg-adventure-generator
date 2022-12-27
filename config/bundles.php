ae3d7056525cff1fbd91b026c8683d2e397128a6
Przemysław Perzyna <przemyslaw.perzyna@polcode.net>
2022-12-27 15:01:25 +0100

    WIP map

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
--- src/Controller/PlayController.php
+++ src/Controller/PlayController.php
@@ -11,6 +11,20 @@ class PlayController extends AbstractController
     #[Route('/play', name: 'app_play')]
     public function index(): Response
     {
-        return $this->render('play/index.html.twig');
+        $rowsCount = 15;
+        $columnsCount = 15;
+
+        $map = array_pad([], $rowsCount, array_pad([], $columnsCount, 0));
+
+        $map[3][5] = 1;
+        $map[rand(0, $rowsCount - 1)][rand(0, $columnsCount-1)] = 1;
+
+        // dump($map);
+
+        return $this->render('play/index.html.twig', [
+            'rows_count' => $rowsCount,
+            'columns_count' => $columnsCount,
+            'map' => $map,
+        ]);
     }
 }
