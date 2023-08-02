44b78bbf0e2d0d33ea56026c2ce5d2b0e6656275
Przemysław Perzyna <przemyslaw.perzyna@polcode.net>
2023-08-02 08:58:08 +0200

    Added MultipleEnemiesEncounterExperienceCountModifier helper

---
+++ src/Helper/MultipleEnemiesEncounterExperienceCountModifier.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Helper;
+
+class MultipleEnemiesEncounterExperienceCountModifier
+{
+    private static array $table = [
+        //COUNT => MULTIPLIER
+        1 => 1.0,
+        2 => 1.5,
+        3 => 2.0,
+        4 => 2.0,
+        5 => 2.0,
+        6 => 2.0,
+        7 => 2.5,
+        8 => 2.5,
+        9 => 2.5,
+        10 => 2.5,
+        11 => 3.0,
+        12 => 3.0,
+        13 => 3.0,
+        14 => 3.0,
+        15 => 4.0,
+    ];
+
+    public static function getMultiplier(int $enemiesCount): float
+    {
+        return self::$table[$enemiesCount];
+    }
+
+    public static function adjustExperiencePoints(int $enemiesCount, int $experiencePoints): int
+    {
+        return self::getMultiplier($enemiesCount) * $experiencePoints;
+    }
+}
