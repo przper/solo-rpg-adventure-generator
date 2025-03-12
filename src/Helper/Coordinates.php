fdfab334d60992a54c0f2305891c35443bf97831
Przemyslaw Perzyna <avan.radien@gmail.com>
2025-03-12 13:08:54 +0100

    Refactor Coordinates to be immutable and readonly
    Converted the `Coordinates` class to an immutable, readonly structure. Removed setter methods and updated `moveBy` to return a new instance. Adjusted tests accordingly to align with the new implementation.

--- src/Helper/Coordinates.php
+++ src/Helper/Coordinates.php
@@ -4,22 +4,22 @@ namespace App\Helper;
 
 use JsonSerializable;
 
-class Coordinates implements JsonSerializable
+final readonly class Coordinates implements JsonSerializable
 {
-    private int $x;
+    private function __construct(
+        private int $x,
+        private int $y,
+    ) {
+    }
 
-    private int $y;
+    public static function fromIntegers(int $x, int $y): self
+    {
+        return new self($x, $y);
+    }
 
     public function getX(): int
     {
         return $this->x;
-    }
-
-    public function setX(int $x): self
-    {
-        $this->x = $x;
-
-        return $this;
     }
 
     public function getY(): int
@@ -27,27 +27,9 @@ class Coordinates implements JsonSerializable
         return $this->y;
     }
 
-    public function setY(int $y): self
-    {
-        $this->y = $y;
-
-        return $this;
-    }
-
-    public function setXY(int $x, int $y): self
-    {
-        $this->x = $x;
-        $this->y = $y;
-
-        return $this;
-    }
-
     public function moveBy(int $deltaX, int $deltaY): self
     {
-        $this->x += $deltaX;
-        $this->y += $deltaY;
-
-        return $this;
+        return new self($this->x + $deltaX, $this->y + $deltaY);
     }
 
     public function jsonSerialize(): mixed
@@ -56,16 +38,6 @@ class Coordinates implements JsonSerializable
             'x' => $this->x,
             'y' => $this->y
         ];
-    }
-
-    public static function fromIntegers(int $x, int $y): self
-    {
-        $coordinates = new self();
-
-        $coordinates->setX($x);
-        $coordinates->setY($y);
-
-        return $coordinates;
     }
 
     public function __toString(): string
--- tests/Unit/Helper/CoordinatesTest.php
+++ tests/Unit/Helper/CoordinatesTest.php
@@ -8,41 +8,6 @@ use PHPUnit\Framework\TestCase;
 class CoordinatesTest extends TestCase
 {
     /** @test */
-    public function it_has_x_and_y()
-    {
-        $coordinates = new Coordinates();
-
-        $coordinates->setX(0);
-        $coordinates->setY(1);
-
-        $this->assertEquals(0, $coordinates->getX());
-        $this->assertEquals(1, $coordinates->getY());
-    }
-
-    /** @test */
-    public function x_and_y_setter()
-    {
-        $coordinates = new Coordinates();
-
-        $coordinates->setXY(0, 1);
-
-        $this->assertEquals(0, $coordinates->getX());
-        $this->assertEquals(1, $coordinates->getY());
-    }
-
-    /** @test */
-    public function it_can_be_moved()
-    {
-        $coordinates = new Coordinates();
-
-        $coordinates->setXY(10, 11);
-        $coordinates->moveBy(-1, -1);
-        
-        $this->assertEquals(9, $coordinates->getX());
-        $this->assertEquals(10, $coordinates->getY());
-    }
-
-    /** @test */
     public function it_can_be_created_from_pair_of_integers()
     {
         $coordinates = Coordinates::fromIntegers(0, 1);
@@ -52,12 +17,19 @@ class CoordinatesTest extends TestCase
     }
 
     /** @test */
+    public function it_can_be_moved()
+    {
+        $coordinates = Coordinates::fromIntegers(10, 11);
+        $coordinates = $coordinates->moveBy(-1, -1);
+
+        $this->assertEquals(9, $coordinates->getX());
+        $this->assertEquals(10, $coordinates->getY());
+    }
+
+    /** @test */
     public function it_is_json_serializable()
     {
-        $coordinates = new Coordinates();
-
-        $coordinates->setX(0);
-        $coordinates->setY(1);
+        $coordinates = Coordinates::fromIntegers(0, 1);
 
         $this->assertJson($coordinates);
         $this->assertJsonStringEqualsJsonString(
@@ -69,11 +41,8 @@ class CoordinatesTest extends TestCase
     /** @test */
     public function it_can_be_printed()
     {
-        $coordinates = new Coordinates();
+        $coordinates = Coordinates::fromIntegers(0, 1);
 
-        $coordinates->setX(0);
-        $coordinates->setY(1);
-        
         $this->assertEquals("[0, 1]", $coordinates);
     }
 }
