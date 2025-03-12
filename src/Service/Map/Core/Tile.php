e612543025f5f3eec6dbbe32cd641d6c416bc4d3
Przemyslaw Perzyna <avan.radien@gmail.com>
2025-03-12 13:11:43 +0100

    Created immutable Tile VO

---
+++ src/Service/Map/Core/Tile.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Service\Map\Core;
+
+use App\Helper\Coordinates;
+use App\Interface\MapCellInterface;
+
+abstract readonly class Tile implements MapCellInterface
+{
+    public function __construct(
+        private Coordinates $coordinates,
+    ) {
+    }
+
+    public function getCoordinates(): Coordinates
+    {
+        return $this->coordinates;
+    }
+
+    abstract public function getType(): string;
+}
