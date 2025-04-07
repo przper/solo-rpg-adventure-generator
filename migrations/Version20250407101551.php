d9a62349bc1a4ccfac753a7d66690a81db636efb
Przemyslaw Perzyna <avan.radien@gmail.com>
2025-04-07 12:39:54 +0200

    migration for vector_embedding & description

---
+++ migrations/Version20250407101551.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250407101551 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE EXTENSION IF NOT EXISTS vector');
+
+        $this->addSql(<<<'SQL'
+            ALTER TABLE monster_shadowdark ADD description TEXT DEFAULT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE monster_shadowdark ADD vector_embedding vector(1536) DEFAULT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN monster_shadowdark.vector_embedding IS '(DC2Type:vector_embedding)'
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql(<<<'SQL'
+            CREATE SCHEMA public
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE monster_shadowdark DROP description
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE monster_shadowdark DROP vector_embedding
+        SQL);
+        $this->addSql('DROP EXTENSION IF EXISTS vector');
+    }
+}
