cae2cf1bc17dc25e12e1a7ef1dd0d5b49c22ea6d
Przemyslaw Perzyna <avan.radien@gmail.com>
2025-04-09 11:12:31 +0200

    Update symfony to 6.4

--- src/Game/Controller/NewGameController.php
+++ src/Game/Controller/NewGameController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class NewGameController extends AbstractController
 {
