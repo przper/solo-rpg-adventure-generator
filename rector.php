2b08dbed99f9119b74a54e0e67855c1ffb868de1
Przemyslaw Perzyna <avan.radien@gmail.com>
2025-04-09 11:36:25 +0200

    Update symfony to 7.2

--- rector.php
+++ rector.php
@@ -1,10 +1,18 @@
 <?php
 
 use Rector\Config\RectorConfig;
+use Rector\Symfony\Set\SymfonySetList;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/src',
         __DIR__.'/tests',
+    ])
+    ->withSets([
+//        SymfonySetList::SYMFONY_62, applied 2025-04-09
+//        SymfonySetList::SYMFONY_63, applied 2025-04-09
+//        SymfonySetList::SYMFONY_64, applied 2025-04-09
+//        SymfonySetList::SYMFONY_70, applied 2025-04-09
+//        SymfonySetList::SYMFONY_71, applied 2025-04-09
     ])
 ;
