6203093adfda7b3737b4cc5a25c29d3c66f1ce22
Perzyna Przemyslaw <przemyslaw.perzyna@paysera.com>
2026-01-23 23:23:04 +0100

    Removed auth

--- config/bundles.php
+++ config/bundles.php
@@ -13,6 +13,4 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     OpenAI\Symfony\OpenAIBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
